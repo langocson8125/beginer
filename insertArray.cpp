04b6fd9cec4cbcba2c4f349805bf7fc30b508502
La Ngoc Son <langocson8125@gmail.com>
2017-04-21 20:12:53 +0700

    Update insertArray.cpp

--- insertArray.cpp
+++ insertArray.cpp
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include "stdlib.h"
-#define MAX 1000
-void getArray(int a[MAX][MAX], int m, int n){
+#define MAX 50
+void getArray(int a[][MAX], int m, int n){
 	for(int i=0;i<m;i++){
 		printf("Nhap cac gia tri hang thu %d",i+1);
 		for(int j=0;j<n;j++){
@@ -10,7 +10,7 @@ void getArray(int a[MAX][MAX], int m, int n){
 		printf("\n");
 	}
 }
-void printArray(int a[MAX][MAX], int m, int n){
+void printArray(int a[][MAX], int m, int n){
 	printf("\nCAC GIA TRI TRONG MANG");
 	for(int i=0; i < m;i++){
 		for(int j=0;j<n;j++){
