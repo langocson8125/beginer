1c89f3c8e7141fd0fb4a21bc56f3c79e3a4aea52
La Ngoc Son <langocson8125@gmail.com>
2017-04-21 19:09:53 +0700

    Create insertArray.cpp

---
+++ insertArray.cpp
@@ -0,0 +1,30 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+void insertArray(int a[][], int m, int n){
+	for(int i=0;i<m;i++){
+		printf("Nhap cac gia tri hang thu %d",i+1);
+		for(int j=0;j<n;j++){
+			scanf("%d",&[i][j]);
+		}
+		printf("\n");
+	}
+}
+void printArray(int a[][], int m, int n){
+	printf("\nCAC GIA TRI TRONG MANG");
+	for(int i=0; i < m;i++){
+		for(int j=0;j<n;j++){
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+}
+int main(){
+	int a[m][n], m, n;
+	printf("Nhap so hang: ");
+	scanf("%d", &m);
+	printf("Nhap so cot: ");
+	scanf("%d",&n);
+	insertArray(a,m,n);
+	return 0;
+}
